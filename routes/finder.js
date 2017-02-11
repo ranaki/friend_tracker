a28b0e4e23ab728343ab3db14552d2fdb2536298
rick baker <baker.rickw@gmail.com>
2017-02-11 16:38:20 -0700

    contains search working

--- routes/finder.js
+++ routes/finder.js
@@ -6,15 +6,35 @@ var Friend = require('../models/friend');
 /* GET a friend */
 router.get('/:name', function(req, res, next) {
  console.log("find this: " + req.params.name);
-//  Friend.find({'friend_name': 'req.params.name'}, function(err, friends) {
    Friend.find({'friend_name': req.params.name}, function(err, friends) {
    res.render('friendFind', { friends: friends });
  });
 });
 
 /* GET a friend */
+// router.get('/', function(req, res, next) {
+//  console.log("find this: " + req.params.name);
+//    Friend.find({'friend_name': req.params.name}, function(err, friends) {
+//    res.render('friendFind', { friends: friends });
+//  });
+// });
+
+// router.get('/', function(req, res, next) {
+//  console.log("find this: " + req.query.friend_name);
+//    Friend.find({'friend_name': req.query.friend_name}, function(err, friends) {
+//    res.render('friendFind', { friends: friends });
+//  });
+// });
+
 router.get('/', function(req, res, next) {
- console.log("find this nothing ");
+    var data = req.query.friend_name;
+   Friend.find({'friend_name': new RegExp(data, 'i')}, function(err, friends) {
+   res.render('friendFind', { friends: friends });
+ });
+});
+
+/* GET a friend */
+router.get('/', function(req, res, next) {
    Friend.find({'friend_name': ''}, function(err, friends) {
    res.render('friendFind', { friends: friends });
  });
