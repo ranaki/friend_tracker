9c060b91e75becb9c447dbca382ff2e2445c9eba
rick baker <baker.rickw@gmail.com>
2017-02-11 09:07:38 -0700

    styling stuff

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser'); 
+var methodOverride = require('method-override');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/friendsdb');
 
@@ -18,6 +19,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
